d8232a3686043b894fa42e738ad39dd9d0c03da1
Sajag Acharya <sajag.acharya@gmail.com>
2014-11-18 19:32:33 +0545

    First!!
    Sudoku time

---
+++ src/drkninja/main/SudoMain.java
@@ -0,0 +1,16 @@
+/*
+ * Sudoku Game
+ * Sajag made this!!
+ */
+
+package drkninja.main;
+
+/**
+ *
+ * @author Admin
+ */
+public class SudoMain {
+    public static void main(String args[]){
+        
+    }
+}
